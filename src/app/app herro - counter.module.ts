a71971b893171da890d2ad371a63924f74026d2b
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-20 11:00:05 +0200

    DBZ module

--- src/app/app herro - counter.module.ts
+++ src/app/app herro - counter.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { DbzModule } from './dbz/dbz.module';
+import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, DbzModule],
+  imports: [BrowserModule, CounterModule, HeroesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
